1a2d4440b42311fe3d984bc1975f97b12b9d70a3
Pethrus Jorge <93262321+pethrus089@users.noreply.github.com>
2023-11-18 15:02:54 -0300

    a
    a

--- menuRolar.js
+++ menuRolar.js
@@ -7,50 +7,51 @@ var sec_tecnologias = document.getElementById('sec_tecnologias')
 var sec_projetos = document.getElementById('sec_projetos')
 var sec_contatos = document.getElementById('sec_contatos')
 var sec_Pethrus = document.getElementById('sec_Pethrus')
-var menu1
+var menu1 
 
-
-tecnologias.addEventListener('click', function () {
+tecnologias.addEventListener('click', function() {
     menu1 = document.getElementById('menu')
-    var offsetY = sec_tecnologias.getBoundingClientRect().top + window.pageYOffset - menu1.getBoundingClientRect().bottom;
-    scrollToSmoothly(offsetY, 1000);
-    //console.log(offsetY)
+    if(menu1.getBoundingClientRect().top==0){
+        var offsetY = sec_tecnologias.getBoundingClientRect().top + window.pageYOffset - menu1.getBoundingClientRect().bottom ;
+    }else{
+        var offsetY = sec_tecnologias.getBoundingClientRect().top + window.pageYOffset - menu1.offsetHeight;
+    }
+    scrollToSmoothly(offsetY, 1000); 
+    console.log(menu1.offsetHeight)
 });
 
-projetos.addEventListener('click', function () {
-    let x = 0
+projetos.addEventListener('click', function() {
+    menu1 = document.getElementById('menu')
+    var offsetY = sec_projetos.getBoundingClientRect().top + window.pageYOffset  - menu1.getBoundingClientRect().bottom ; 
+    scrollToSmoothly(offsetY, 1000); 
+});
+
+contatos.addEventListener('click', function() {
+    menu1 = document.getElementById('menu')
+    console.log(menu1.this.getBoundingClientRect().top)
+    var offsetY = sec_contatos.getBoundingClientRect().top + window.pageYOffset  - menu1.getBoundingClientRect().bottom ; 
+    scrollToSmoothly(offsetY, 1000); 
+});
+
+Pethrus.addEventListener('click', function() {
+    menu1 = document.getElementById('menu')
+    console.log(menu1.this.getBoundingClientRect().top)
+    var offsetY = sec_Pethrus.getBoundingClientRect().top + window.pageYOffset  - menu1.getBoundingClientRect().bottom ; 
+    scrollToSmoothly(offsetY, 1000);  
+});
+
+
+/* 
+//nova funcion
+function chegaNoElemento(num){
     do {
-        menu1 = document.getElementById('menu')
-        var offsetY = sec_projetos.getBoundingClientRect().top + window.pageYOffset - menu1.getBoundingClientRect().bottom;
-        scrollToSmoothly(offsetY, 1000);
-        console.log('passei')
-        console.log(sec_projetos.getBoundingClientRect().top + window.pageYOffset - menu1.getBoundingClientRect().bottom);
-        console.log(window.pageYOffset)
-        x++
-        // Using an anonymous function
-        setTimeout(function () {
-            menu1 = document.getElementById('menu')
-            offsetY = sec_projetos.getBoundingClientRect().top + window.pageYOffset - menu1.getBoundingClientRect().bottom;
-            scrollToSmoothly(offsetY, 1000);
-            console.log('passei')
-            console.log(sec_projetos.getBoundingClientRect().top + window.pageYOffset - menu1.getBoundingClientRect().bottom);
-            console.log(window.pageYOffset)
-        }, 100);
+        num = sec_Pethrus.getBoundingClientRect().top + window.pageYOffset  - menu1.getBoundingClientRect().bottom
+        scrollToSmoothly(num, 1000); 
+    } while (num=!sec_Pethrus.getBoundingClientRect().top + window.pageYOffset  - menu1.getBoundingClientRect().bottom)
+}
 
-    } while (x < 8)
-});
+ */
 
-contatos.addEventListener('click', function () {
-    menu1 = document.getElementById('menu')
-    var offsetY = sec_contatos.getBoundingClientRect().top + window.pageYOffset - menu1.getBoundingClientRect().bottom;
-    scrollToSmoothly(offsetY, 1000);
-});
-
-Pethrus.addEventListener('click', function () {
-    menu1 = document.getElementById('menu')
-    var offsetY = sec_Pethrus.getBoundingClientRect().top + window.pageYOffset - menu1.getBoundingClientRect().bottom;
-    scrollToSmoothly(offsetY, 1000);
-});
 
 function scrollToSmoothly(targetY, duration) {
     const startingY = window.pageYOffset;
